c95abfd53cf752d16d757acd509584d4fc002ab1
annedb20 <165612402+annedb20@users.noreply.github.com>
2024-08-25 18:34:31 -0400

    Update Stat_4_Top_15_Frequency_Share.R
    Make compatible with new PCSC dataset

--- scripts/5-stats/Stat_4_Top_15_Frequency_Share.R
+++ scripts/5-stats/Stat_4_Top_15_Frequency_Share.R
@@ -1,7 +1,15 @@
 source("packages.R")
 source("1-load_data.R")
 source("2-clean_data.R")
-source("Fig_4_PCSC_Practices.R")
+
+# Create a function which runs code snippets to avoid redundantly downloading data
+source2 <- function(file, start, end, ...) {
+  file.lines <- scan(file, what=character(), skip=start-1, nlines=end-start+1, sep='\n')
+  file.lines.collapsed <- paste(file.lines, collapse='\n')
+  source(textConnection(file.lines.collapsed), ...)
+}
+
+source2("Fig_4_PCSC_Practices.R", start = 4, end = 59)
 
 # Statistic 4: Frequency of top 15 PCSC practices
 
