05d8478195e23b0101a6f59dd9da2acf8f3ce2a6
annedb20 <165612402+annedb20@users.noreply.github.com>
2024-08-25 18:37:45 -0400

    Update Stat_8_HUP_Support_Across_Partners.R
    Make compatible with new PCSC dataset

--- scripts/5-stats/Stat_8_HUP_Support_Across_Partners.R
+++ scripts/5-stats/Stat_8_HUP_Support_Across_Partners.R
@@ -1,23 +1,26 @@
 source("packages.R")
 source("1-load_data.R")
 source("2-clean_data.R")
-source("Stat_6_MMRV_by_Partner_Type.R")
+
+# Create a function which runs select code without redownloading data
+source2 <- function(file, start, end, ...) {
+  file.lines <- scan(file, what=character(), skip=start-1, nlines=end-start+1, sep='\n')
+  file.lines.collapsed <- paste(file.lines, collapse='\n')
+  source(textConnection(file.lines.collapsed), ...)
+}
+
+source2("Stat_6_MMRV_by_Partner_Type.R", start = 4, end = 82)
 
 # Statistic 8: HUP support across partner types
 
-# Merge actor data with HUP data for PCSC
-PCSC_HUP_and_actors <- left_join(PCSC_data_with_HUP_info, PCSC_Partner_Types, by = "Applicant")
-PCSC_HUP_by_actor <- PCSC_HUP_and_actors %>%
+# Count HUP support for different partner types
+PCSC_HUP_by_actor <- PCSC_Projects %>%
   filter(`Primarily HUP` == 1) %>%
-  group_by(`Actor Type`) %>%
+  group_by(Actor.Type) %>%
   summarize(count_primarily_HUP = n())
 
-# Rename column for merging
-freq_actors_df <- freq_actors_df %>%
-  rename(`Actor Type` = `Actor.Type`)
-
 # Add in actor frequencies for weighting
-PCSC_HUP_by_actor_weighted <- inner_join(PCSC_HUP_by_actor, freq_actors_df, by = "Actor Type")
+PCSC_HUP_by_actor_weighted <- full_join(PCSC_HUP_by_actor, freq_actors_df, by = "Actor.Type")
 PCSC_HUP_by_actor_weighted <- PCSC_HUP_by_actor_weighted %>%
   mutate(weighted_primarily_HUP = count_primarily_HUP / Freq * 100)
 
