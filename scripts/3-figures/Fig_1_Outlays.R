1577051adef32b074f20d67b5aaf7945e4ee9c8c
benamie <elinor.benami@gmail.com>
2025-02-21 22:06:30 -0500

    shorten labels, adjust annotation locations, add height param to export, compress legend, move legend title to the top

--- scripts/3-figures/Fig_1_Outlays.R
+++ scripts/3-figures/Fig_1_Outlays.R
@@ -21,17 +21,17 @@ program_funding_plot <-
                                "RCPP Farm Bill" = "#efedf5",
                                "CSP Farm Bill" = "#bcbddc",
                                "EQIP Farm Bill" = "#756bb1"),
-                    labels = c("IRA: Regional Cons. Partnership Program", 
-                                "IRA: Cons. Stewardship Program",
-                                "IRA: Enviro. Quality Incentives Program",
-                                "Farm Bill: Regional Cons. Partnership Program",
-                                "Farm Bill: Cons. Stewardship Program",
-                                "Farm Bill: Enviro. Quality Incentives Program")) +
+                    labels = c("IRA: Regional Cons. Partnership", 
+                                "IRA: Cons. Stewardship",
+                                "IRA: Enviro. Quality Incentives",
+                                "Farm Bill: Regional Cons. Partnership",
+                                "Farm Bill: Cons. Stewardship",
+                                "Farm Bill: Enviro. Quality Incentives")) +
   # Add text labelling IRA and Farm Bill categories
   annotate("text", x = 2028.75, y = 1.5, label = "Farm Bill\nFunding", hjust = 0.5,
-           vjust = 1, color = "white", size = 5, angle = 0, fontface="bold" ) +
-  annotate("text", x = 2028.75, y = 4.25, label = "Inflation Reduction \nAct Funding", hjust = 0.5,
-           vjust = 1, color = "white", size = 5, angle = 0, fontface="bold") +
+           vjust = 1, color = "white", size = 4, angle = 0, fontface="bold" ) +
+  annotate("text", x = 2028.75, y = 4.35, label = "Inflation Reduction \nAct Funding", hjust = 0.5,
+           vjust = 1, color = "white", size = 4, angle = 0, fontface="bold") +
   # Year labels starting from 2023 and every two years thereafter
   scale_x_continuous(breaks = seq(min(Program_Funding_and_Outlays$Year), 
                                   max(Program_Funding_and_Outlays$Year), by = 1),
@@ -41,16 +41,21 @@ program_funding_plot <-
                      breaks = 1:7) + # Set expand to zero for the y-axis
   theme_light(base_size = 14) + 
   theme(legend.position = "bottom",
-        axis.text.y = element_text(angle = 0, vjust = 0.5, size = 14), # Larger text size for axis labels
-        axis.title.y = element_text(angle = 0, vjust = 0.5, size = 14, face = "bold"), # Slightly smaller text size for title
-        legend.title = element_text(size = 12, face="bold"),
-        legend.text = element_text(size = 11),   # Larger text size for legend labels
-        axis.text.x = element_text(angle = 0, vjust = 0.5, size = 14), # Apply larger text size for x-axis labels
-        axis.title.x = element_text(angle = 0, hjust = 0.5, size = 14, face = "bold"),
+        axis.text.y = element_text(angle = 0, vjust = 0.5, size = 11),
+        axis.title.y = element_text(angle = 0, vjust = 0.5, size = 12, face = "bold"), 
+        legend.title = element_text(size = 12, face="bold", hjust = 0.5),
+        legend.title.align = 0.5,    # Center the title relative to legend box
+        legend.box = "horizontal",  # Layout legend items horizontally
+        legend.key.width = unit(1.5, "lines"),  # Adjust key width, optional
+        legend.key.height = unit(1, "lines"),  # Adjust key height, optional
+        legend.margin = margin(t = -15, b = 5, l = 0, r = 0),  # Reduce margin around the legend to decrease the gap
+        legend.text = element_text(size = 10),   # Larger text size for legend labels
+        axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11), 
+        axis.title.x = element_text(angle = 0, hjust = 0.5, size = 12, face = "bold"),
         text = element_text(family = "sans") # Apply text size for x-axis title
         ) + 
-  guides(fill = guide_legend(ncol = 2)) # Adjust legend column number to 2
+  guides(fill = guide_legend(title.position = "top", title.hjust = 0.5, ncol = 2))
 
 program_funding_plot
 
-ggsave.latex(program_funding_plot, filename = file_path("figs/outlays_v2.pdf"))
+ggsave.latex(program_funding_plot, filename = file_path("figs/outlays_v2.pdf"), height = 4, width = 6.7, units = "in")
