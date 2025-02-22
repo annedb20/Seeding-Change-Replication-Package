399ee4ce44c32d5c2c02072b562bd854804c944a
annedb20 <165612402+annedb20@users.noreply.github.com>
2025-02-22 14:34:39 -0500

    Update Tab_2_Totals_per_Program.R
    Remove CSP-GCI and sum RCPP categories

--- scripts/4-tables/Tab_2_Totals_per_Program.R
+++ scripts/4-tables/Tab_2_Totals_per_Program.R
@@ -8,12 +8,25 @@ source("2-clean_data.R")
 totals_program <- Contract_Political_Download %>%
   filter(geography_level == "National", obligation_fy == "Total",
          fips_code == "Total", historically_underserved == "Total",
-         !program %in% c("AWEP", "AMA", "WHIP")) %>%
+         !program %in% c("AWEP", "AMA", "WHIP", "CSP-GCI")) %>%
   group_by(program) %>%
   # Due to lack of contract_status column, must filter for max of dollars_obligated to filter for "Total" as contract_status
   filter(dollars_obligated == max(dollars_obligated, na.rm = TRUE)) %>%
   ungroup() %>%
+  # Sum RCPP categories
+  mutate(
+    program_short = case_when(
+      str_detect(program, "RCPP") ~ "RCPP",
+      TRUE ~ program)) %>% # Keep the original value if no match
+  select(-program) %>%
+  rename(program = program_short) %>%
+  group_by(program) %>%
+  mutate(dollars_obligated = sum(dollars_obligated),
+         contract_count = sum(contract_count),
+         treated_acres = sum(treated_acres)) %>%
+  ungroup() %>%
   select(program, dollars_obligated, contract_count, treated_acres) %>%
+  distinct() %>%
   arrange(-dollars_obligated, program) %>%
   # Add formatting for output
   mutate(across(where(is.numeric), ~scales::comma(.))) %>%  # Add commas to numeric values
