c8494d3c77740d3fd74dd70cb097f9395ee095f0
benamie <elinor.benami@gmail.com>
2025-02-23 12:52:10 -0500

    Add preamble and ggsave latex, add scales to show commas in text, remove titles, move legend into graphic, deduplicate the df so text appears, add consistent legend themes, shorten x axis text and increase size for legibility, add in full program name

--- scripts/3-figures/Fig_6_HUP_Share.R
+++ scripts/3-figures/Fig_6_HUP_Share.R
@@ -1,11 +1,21 @@
-source("packages.R")
-source("1-load_data.R")
-source("2-clean_data.R")
+########################################################################
+# Author: Anne Bell Carroll, Elinor Benami (elinor@vt.edu)
+# Date: # Sat Feb 22 14:26:43 2025 ------------------------------
+# Purpose: Gen Figure on Share of Dollars Dedicated to HUPs by Program and Contract Status
+# Notes:
+########################################################################
 
-# Figure 6: Share of Dollars Dedicated to HUPs by Program and Contract Status
+# source("packages.R")
+# source("1-load_data.R")
+# source("2-clean_data.R")
+
+# Add script specific packages
+library(tidylog)
+library(scales)
 
 # Create new dataset representing HUP shares of dollars
-hup_share_dollars <- Contract_Download_Table_HUP %>% 
+hup_share_dollars <- 
+  Contract_Download_Table_HUP %>% 
   filter(contract_fiscal_year != "Total") %>%
   group_by(program_short, contract_status, hist._underserved) %>%
   # Generate a variable that is the sum of dollars_obligated for each program, contract status, and HUP status
@@ -24,14 +34,27 @@ hup_share_dollars <- Contract_Download_Table_HUP %>%
   mutate(sum_contract_counts = sum(contract_count, na.rm = TRUE)) %>%
   ungroup()
 
-# Identify minimum year
-min_year <- min(Contract_Download_Table_HUP$contract_fiscal_year)
+# Whittle down hup file to only include relevant columns and that they are distinct
+hup_share_dollars_distinct <-  hup_share_dollars %>% distinct(program_short, contract_status, share_hist_underserved, sum_contract_counts)
+ 
+# Define a named vector with program abbreviations and their full names
+program_names <- c(
+  EQIP = "Environmental.\nQuality Incentives\nProgram (EQIP)",
+  CSP = "Conservation\nStewardship\nProgram (CSP)",
+  RCPP = "Regional\nConservation\nPartnership\nProgram (RCPP)"
+)
 
-# Create the bar chart
-hup_share_dollars_plot <- ggplot(hup_share_dollars, aes(x = share_hist_underserved, 
-                                                        y = program_short,
-                                                        fill = factor(contract_status, 
-                                                                      levels = c("Terminated", "Completed", "Cancelled", "Active")))) +
+# Create the bar chart ----- 
+hup_share_dollars_plot <- 
+  hup_share_dollars_distinct %>% 
+  ggplot(aes(
+    x = share_hist_underserved,
+    y = program_short,
+    fill = factor(
+      contract_status,
+      levels = c("Terminated", "Completed", "Cancelled", "Active")
+    )
+  )) +
   geom_col(position = position_dodge(width = 0.75), color = "white", width = 0.7) +
   scale_fill_manual(values = c("Active" = "#a6dba0", 
                                "Cancelled" = "#f4a582", # Mutually agreeable
@@ -39,23 +62,36 @@ hup_share_dollars_plot <- ggplot(hup_share_dollars, aes(x = share_hist_underserv
                                "Completed" = "#018571"),
                     guide = guide_legend(reverse = TRUE)) + 
   labs(fill = "Contract Status", 
-       y = "", x = "Share of Dollars in a Given Status Associated with Historically Underserved Producers", 
-       title = "Share of Dollars Dedicated to Historically Underserved Producers by Program and Contract Status",
-       subtitle = paste0("Fiscal Year ", min_year, " - 2023"),
-       caption = "Data Source: USDA NRCS Conservational Financial Assistance Data") +
+       y = "", 
+       x = "\n Share of funds linked to Historically Underserved Producers"
+       # title = "Share of Dollars Dedicated to Historically Underserved Producers by Program and Contract Status",
+       # subtitle = paste0("Fiscal Year ", min_year, " - 2023"),
+       # caption = "Data Source: USDA NRCS Conservational Financial Assistance Data"
+       ) +
   guides(fill = guide_legend(reverse = TRUE)) +
   scale_x_continuous(limits = c(0, 100), breaks = seq(0, 100, 10), expand = c(0, 0)) +
   # start the y axis at the 0 line
-  scale_y_discrete(expand = c(0, 0)) +
-  geom_vline(xintercept = 40, linetype = "dashed", color = "blue") +
-  geom_text(aes(label = sum_contract_counts), position = position_dodge(width = .75),
-            hjust = -0.1, vjust = 0.5, show.legend = F, size = 5) + # Add labels, adjust size and position, and remove from legend
-  theme_classic() + # Increase font size
-  theme(plot.caption = element_text(hjust = 0),
-        legend.position = "bottom",
-        panel.grid.major = element_blank(), # Remove major grid lines
-        panel.grid.minor = element_blank(),
-        text = element_text(size = 20),
-        plot.margin = margin(1, 1, 1, 0, "cm")) # Increase base font size for all text elements
+  scale_y_discrete(expand = c(0, 0), labels = program_names) +
+  geom_vline(xintercept = 40, linetype = "dashed", color = "#5A5A5A", linewidth = 0.75) +
+  geom_text(aes(label = scales::comma(sum_contract_counts)), 
+            position = position_dodge(width = .75),
+            hjust = -0.1, vjust = 0.5, fontface = "bold",
+            show.legend = F, size = 3.5) + # Add labels, adjust size and position, and remove from legend
+  theme_classic() +
+  theme(axis.text.y = element_text(angle = 0, hjust = 0.5, # center text
+                                   vjust = 0.5, size = 10.5, face = "bold"),
+        axis.title.y = element_text(angle = 0, vjust = 0.5, size = 12, face = "bold"), 
+        axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11.5), 
+        axis.title.x = element_text(angle = 0, hjust = 0.5, size = 12, face = "bold"),
+        # legend opts
+        legend.position = c(0.8, 0.82),  # Inset the Contract Status
+        # legend.position = "bottom",
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 11.5, face = "bold"),
+        legend.key.size = unit(0.5, "cm"),
+        text = element_text(family = "sans"),
+        # settings for legend background
+        legend.background = element_rect(colour = "gray", size = 0.5)
+  ) 
 
 hup_share_dollars_plot
