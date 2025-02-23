2e2a0cea79dc9bcbe5245f691049dda08d0ad9b6
benamie <elinor.benami@gmail.com>
2025-02-23 01:25:36 -0500

    Add preamble, ggsave_latex, strip out leading white spaces, compress text, increase font size, remove title, change color, move graph start to origin, export graphic

--- scripts/3-figures/Fig_5_PCSC_Practices.R
+++ scripts/3-figures/Fig_5_PCSC_Practices.R
@@ -1,21 +1,29 @@
-source("packages.R")
-source("1-load_data.R")
-source("2-clean_data.R")
+########################################################################
+# Author: Anne Bell Carroll, Elinor Benami (elinor@vt.edu)
+# Date: # Sat Feb 22 14:26:43 2025 ------------------------------
+# Purpose: Gen Figure on  Top 15 PCSC Practices by Frequency
+# Notes:
+########################################################################
 
-# Figure 5: Top 15 PCSC Practices
+# source("packages.R")
+# source("1-load_data.R")
+# source("2-clean_data.R")
+# source("scripts/ggsave_latex.R")
 
 # Calculate total number of projects (labeled "Applicant" in the data)
 total_applicants <- n_distinct(PCSC_Projects$Applicant)
 
 # Split Available_Practices into separate columns for each practice
-Partnerships_for_CS_Commodities_data_split <- PCSC_Projects %>%
+Partnerships_for_CS_Commodities_data_split <- 
+  PCSC_Projects %>%
   rename(Available_Practices = 'Available Practices') %>% # Rename for easier manipulation
   separate_rows(Available_Practices, sep = ",") %>% # Create different rows
   pivot_wider(names_from = Available_Practices, values_from = Available_Practices, # Change into columns
               values_fn = length, values_fill = list(Available_Practices = 0))
 
 # Add up instances of eligibility for each practice
-practice_counts <- Partnerships_for_CS_Commodities_data_split %>%
+practice_counts <-
+  Partnerships_for_CS_Commodities_data_split %>%
   summarize(across(.cols = -Applicant, ~sum(. == 1, na.rm = TRUE))) %>%
   # Remove non-practice columns
   select(-`Project Summary`, 
@@ -47,17 +55,38 @@ practice_counts <- Partnerships_for_CS_Commodities_data_split %>%
 colnames(practice_counts) <- gsub("\\d+", "", colnames(practice_counts))
 
 # Sort in descending order of frequency
-frequency_practices <- practice_counts %>%
+frequency_practices <- 
+  practice_counts %>%
   pivot_longer(cols = everything(), names_to = "Practice", values_to = "Count") %>%
-  arrange(desc(Count))
+  arrange(desc(Count)) %>% 
+  # remove the words "Residue and Tillage Management -" from the Practice List (but keep anything that comes after it )
+  mutate(Practice = str_remove(Practice, "Residue And Tillage Management -")) %>% 
+  # strip out leading white spaces before all practices
+  mutate(Practice = str_trim(Practice)) 
 
 # Graph the results with a stacked bar chart
-PCSC_frequency_practices_plot <- frequency_practices %>%
+PCSC_frequency_practices_plot <- 
+  frequency_practices %>%
+  filter(Practice != "Primarily HUP") %>% 
   slice(1:15) %>% # Take top 15
   ggplot(aes(x = Count, y = reorder(Practice, Count))) + 
-  geom_bar(stat = "identity", fill = "skyblue") + 
-  theme_minimal(base_size = 20) +
-  labs(title = "Top 15 PCSC Practices by Frequency",
-       x = "Number of PCSC Projects for which a Practice is Eligible", y = "")
+  geom_bar(stat = "identity", fill = "#0c2458") + 
+  theme_minimal() +
+  scale_x_continuous(expand = c(0, 0),
+                     #breaks every 5 units+
+                     breaks = seq(0, 95, by = 15)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  labs(#title = "Top 15 PCSC Practices by Frequency",
+       x = "\nNumber of PCSC Projects for which a practice is Eligible", y = "") +
+  theme(axis.text.y = element_text(angle = 0, vjust = 0.5, size = 11),
+        axis.title.y = element_text(angle = 0, vjust = 0.5, size = 11, face = "bold"), 
+        axis.text.x = element_text(angle = 0, vjust = 0.5, size = 11), 
+        axis.title.x = element_text(angle = 0, hjust = 0.5, size = 11, face = "bold"),
+        text = element_text(family = "sans") # Apply text size for x-axis title
+  ) 
 
 PCSC_frequency_practices_plot
+
+ggsave.latex(PCSC_frequency_practices_plot, 
+             filename = file_path("figs/PCSC_practices_v2.pdf"), 
+             width = 6.5, height = 3.5, units = "in")
