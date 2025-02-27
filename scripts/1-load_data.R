11e11017a1da61cc29a2fc3a67757dcfd657d28e
annedb20 <165612402+annedb20@users.noreply.github.com>
2025-02-27 13:40:49 -0500

    Add Census Data

--- scripts/1-load_data.R
+++ scripts/1-load_data.R
@@ -1,3 +1,10 @@
+########################################################################
+# Author: Anne Bell Carroll (annedb20@vt.edu), Elinor Benami (elinor@vt.edu)
+# Date: # Sat Feb 27 12:50:43 2025 ------------------------------
+# Purpose: Load data from the dataverse
+# Notes:
+########################################################################
+
 source("packages.R")
 
 # Download the data from the dataverse
@@ -66,6 +73,42 @@ Program_Funding_and_Outlays <- get_dataframe_by_name(
   original = TRUE,
   .f = readr::read_delim
 )
+
+# Census_No._Operations
+Census_No._Crop_Operations <- get_dataframe_by_name(
+  filename = "Census_No._Operations.tab",
+  dataset = "https://doi.org/10.7910/DVN/MMXD4Y",
+  server = "dataverse.harvard.edu",
+  original = TRUE,
+  .f = readr::read_delim
+  )
+
+#	Census_Cropland_Acres
+Census_Cropland_Acres <- 	get_dataframe_by_name(
+  filename = "Census_Total_Cropland_Acres.tab",
+  dataset = "https://doi.org/10.7910/DVN/MMXD4Y",
+  server = "dataverse.harvard.edu",
+  original = TRUE,
+  .f = readr::read_delim
+  )
+
+# Census_Cover_Crop
+Census_Cover_Crop <- get_dataframe_by_name(
+  filename = "Census_Cover_Crop.tab",
+  dataset = "https://doi.org/10.7910/DVN/MMXD4Y",
+  server = "dataverse.harvard.edu",
+  original = TRUE,
+  .f = readr::read_delim
+  )
+
+# Census_Tillage
+Census_Tillage <- get_dataframe_by_name(
+  filename = "Census_Reduced_and_No_Till.tab",
+  dataset = "https://doi.org/10.7910/DVN/MMXD4Y",
+  server = "dataverse.harvard.edu",
+  original = TRUE,
+  .f = readr::read_delim
+  )
 
 # XLSX File
 # Program_Funding_Levels
