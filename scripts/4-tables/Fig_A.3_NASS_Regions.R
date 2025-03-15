6fbd29c935f1b0a07d5ac1c44d638ea65c24a747
annedb20 <165612402+annedb20@users.noreply.github.com>
2025-03-14 22:13:37 -0400

    Clean and update figure number

--- scripts/4-tables/Fig_A.3_NASS_Regions.R
+++ scripts/4-tables/Fig_A.3_NASS_Regions.R
@@ -2,7 +2,7 @@ source("packages.R")
 source("1-load_data.R")
 source("2-clean_data.R")
 
-# Table 4: NASS Regions
+# Fig A.3: NASS Regions
 # NASS regions derived from https://www.nass.usda.gov/Statistics_by_State/RFO/index.php
 
 # Create region variable
@@ -30,37 +30,9 @@ States_Territories <- c("Pennsylvania, Delaware, Maryland, New Jersey, New York,
 # Create dataframe integrating these two variables
 NASS_Regions <- data.frame(Region = Region, State_Territories = States_Territories)
 
-# Read in map data
-library(sf)
-us_map <- read_sf("/Users/annedb20/Downloads/states/states.geojson")
-us_map <- us_map %>%
+# Remove D.C.
+us_map <- us_states %>%
   filter(NAME != "District of Columbia")
-
-# Shift locations of Alaska and Hawaii
-alaska <- us_map %>%
-  filter(NAME %in% "Alaska")
-alaska_g <- st_geometry(alaska)
-alaska_centroid <- st_centroid(st_union(alaska_g))
-
-# Rotation function
-rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
-
-alaska_trans <- (alaska_g - alaska_centroid) * rot(-1 * pi/180) / 2.3 + alaska_centroid + c(35, -40)
-
-alaska <- alaska %>% st_set_geometry(alaska_trans) %>% st_set_crs(st_crs(us_map))
-
-hawaii <- us_map %>% filter(NAME %in% 'Hawaii')
-
-hawaii_g <- st_geometry(hawaii)
-hawaii_centroid <- st_centroid(st_union(hawaii_g))
-
-hawaii_trans <- (hawaii_g - hawaii_centroid) * rot(-35 * pi/180) + hawaii_centroid + c(53, 4)
-hawaii <- hawaii %>% st_set_geometry(hawaii_trans) %>% st_set_crs(st_crs(us_map))
-
-us_map_final <- us_map %>%
-  filter(!NAME %in% c('Alaska', 'Hawaii')) %>%
-  rbind(alaska) %>%
-  rbind(hawaii)
 
 # Define regions
 Region <- c("Northeastern", "Northeastern", "Northeastern", "Northeastern", "Northeastern", 
@@ -100,7 +72,7 @@ NASS_Regions_2 <- data.frame(Region, NAME)
 
 # Combine dataframe of regional groupings with the map
 
-us_map_regions <- us_map_final %>%
+us_map_regions <- us_map %>%
   left_join(NASS_Regions_2, by = "NAME") %>%
   mutate(Region = factor(Region, levels = c("Northeastern", "Eastern Mountain",
                                             "Southern", "Great Lakes", "Upper Midwest",
